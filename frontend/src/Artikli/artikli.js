bdc5f64e9e36d3860c1c0f5b2d2aea9f3c9aecbd
zaneticIT <petar.zanetic.it@gmail.com>
2024-04-17 03:29:45 +0200

    migrate "artikli" to use same format as "ugovori"

--- frontend/src/Artikli/artikli.js
+++ frontend/src/Artikli/artikli.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {
-  Typography,
-  CircularProgress,
-  List,
-  ListItem,
-  ListItemText,
-  Container,
-} from "@mui/material";
+import { CircularProgress, Paper, Container } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 
 function Artikli() {
   const [artikli, setArtikli] = useState(null);
@@ -28,22 +27,57 @@ function Artikli() {
 
   return (
     <Container maxWidth="md">
-      <Typography variant="h4" gutterBottom>
-        API Data
-      </Typography>
-      {artikli ? (
-        <List>
-          {artikli.map((item, index) => (
-            <ListItem key={index}>
-              <ListItemText primary={JSON.stringify(item)} />
-            </ListItem>
-          ))}
-        </List>
-      ) : (
-        <div style={{ textAlign: "center" }}>
-          <CircularProgress />
-        </div>
-      )}
+      <Paper
+        variant="outlined"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: artikli ? "" : "25vh",
+        }}
+      >
+        {artikli ? (
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Naziv</TableCell>
+                  <TableCell align="right">Dobavljač</TableCell>
+                  <TableCell align="right">Status</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {artikli.map((item, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                      backgroundColor:
+                        item.status === "KREIRANO"
+                          ? "#66bb6a"
+                          : item.status === "NARUČENO"
+                          ? "#ffa726"
+                          : item.status === "ISPORUČENO"
+                          ? "grey"
+                          : "",
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {item.naziv}
+                    </TableCell>
+                    <TableCell align="right">{item.dobavljac}</TableCell>
+                    <TableCell align="right">{item.status}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <span>
+            <CircularProgress />
+          </span>
+        )}
+      </Paper>
     </Container>
   );
 }
